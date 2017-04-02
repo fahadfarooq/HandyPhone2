aba7330aa5324c1b2fd8e222f77426fd748b68fc
fahadfarooq <fahadfarooq54@gmail.com>
2017-04-02 20:17:59 +0500

    Initial commit

---
+++ app/src/main/java/com/example/fahad/handyphone/ImageItem.java
@@ -0,0 +1,33 @@
+package com.example.fahad.handyphone;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by fahad on 4/2/2017.
+ */
+public class ImageItem {
+    private Bitmap image;
+    private String title;
+
+    public ImageItem(Bitmap image, String title) {
+        super();
+        this.image = image;
+        this.title = title;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
